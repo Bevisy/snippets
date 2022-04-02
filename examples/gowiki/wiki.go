48f2e5a85b2c7672427390b173fc0194251fb77b
Binbin Zhang <binbin36520@gmail.com>
2022-04-02 17:58:24 +0800

    add new web application

---
+++ examples/gowiki/wiki.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	fName := p.Title + ".txt"
+
+	return os.WriteFile(fName, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	var err error
+	ret := &Page{}
+	fName := title + ".txt"
+	ret.Title = title
+	ret.Body, err = os.ReadFile(fName)
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+// Template caching
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+// 页面渲染
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
