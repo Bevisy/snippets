7d54903146ebce83c0c94f13ff02f70b82eaf987
Binbin Zhang <binbin36520@gmail.com>
2022-04-03 22:49:11 +0800

    Update go wiki example
    1. add "/" handler
    2. modify directory structure

--- examples/gowiki/wiki.go
+++ examples/gowiki/wiki.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+)
+
+const (
+	templatePath = "tmpl/"
+	dataPath     = "data/"
 )
 
 type Page struct {
@@ -15,7 +22,7 @@ type Page struct {
 func (p *Page) save() error {
 	fName := p.Title + ".txt"
 
-	return os.WriteFile(fName, p.Body, 0600)
+	return os.WriteFile(dataPath+fName, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -23,15 +30,14 @@ func loadPage(title string) (*Page, error) {
 	ret := &Page{}
 	fName := title + ".txt"
 	ret.Title = title
-	ret.Body, err = os.ReadFile(fName)
+	ret.Body, err = os.ReadFile(dataPath + fName)
 	if err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -40,8 +46,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -49,8 +54,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -61,20 +65,51 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.Redirect(w, r, "/view/"+"FrontPage", http.StatusFound)
+	}
+}
+
 // Template caching
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles(templatePath+"edit.html", templatePath+"view.html"))
 
 // 页面渲染
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// 参数为 模版名称
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil // The title is the second subexpression.
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
